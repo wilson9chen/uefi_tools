wilson9chen/uefi_tools

commit 0c485dabe18911af0df38c28499b3bc075f4285e

opteed-build.sh: Remove copy of images to UEFI dir
The copy was originally mirrored after atf-build.sh but in restrospect
TOS_BIN doesn't really have to be copied since it's already embedded in
fip.bin and not directly required for programming the platform.

Signed-off-by: Victor Chong <victor.chong@linaro.org>
Reviewed-by: Haojian Zhuang <haojian.zhuang@linaro.org>
Reviewed-by: Fathi Boudra <fathi.boudra@linaro.org>
Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

opteed-build.sh
@@ -103,10 +103,6 @@ function build_platform
 		# Copy resulting images to UEFI image dir
 		#
 		TOS_BIN="`$TOOLS_DIR/parse-platforms.py $PLATFORM_CONFIG -p $1 get -o tos_bin`"
-		if [ $VERBOSE -eq 1 ]; then
-			echo "Copying '$TOS_BIN' to '$EDK2_DIR/Build/$PLATFORM_IMAGE_DIR/$BUILD_PROFILE/FV/'"
-		fi
-		cp -a out/arm-plat-"$TOS_PLATFORM"/core/"$TOS_BIN" "$EDK2_DIR/Build/$PLATFORM_IMAGE_DIR/$BUILD_PROFILE/FV/"
 	else
 		return 1
 	fi

commit b37391801290b4adbbc821832470216e98d4e900

opteed-build.sh: Copy images to UEFI dir
atf-build.sh expects TOS binaries to be available in the UEFI image
dir so copy them there.

Fixes: 0c485dab ("opteed-build.sh: Remove copy of images to UEFI dir")
Signed-off-by: Victor Chong <victor.chong@linaro.org>
Reviewed-by: Fathi Boudra <fathi.boudra@linaro.org>
Reviewed-by: Leif Lindholm <leif.lindholm@linaro.org>

opteed-build.sh
@@ -106,6 +106,19 @@ function build_platform
 		TOS_BIN="`$TOOLS_DIR/parse-platforms.py $PLATFORM_CONFIG -p $1 get -o tos_bin`"
 		TOS_BIN_EXTRA1="`$TOOLS_DIR/parse-platforms.py $PLATFORM_CONFIG -p $1 get -o tos_bin_extra1`"
 		TOS_BIN_EXTRA2="`$TOOLS_DIR/parse-platforms.py $PLATFORM_CONFIG -p $1 get -o tos_bin_extra2`"
+		DESTDIR="$EDK2_DIR/Build/$PLATFORM_IMAGE_DIR/$BUILD_PROFILE/FV/"
+		COREDIR="out/$TOS_ARCH-plat-$TOS_PLATFORM/core"
+		if [ $VERBOSE -eq 1 ]; then
+			echo "Copying TOS binaries to '$DESTDIR'"
+			CPFLAGS="-v"
+		else
+			CPFLAGS=""
+		fi
+		for file in $COREDIR/{"$TOS_BIN","$TOS_BIN_EXTRA1","$TOS_BIN_EXTRA2"}; do
+			if [ -f "$file" ]; then
+				cp -a $CPFLAGS $file "$DESTDIR"
+			fi
+		done
 	else
 		return 1
 	fi